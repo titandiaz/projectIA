ca7d471b3b9ef524679abcc188a559b86f5f7330
DESKTOP-3QJGL4M\titan <titanmar15@gmail.com>
2018-10-23 14:47:10 -0500

    primer commit

---
+++ components/card-twitter.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="container">
+  <div class="row">
+    <div class="col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6">
+      <div class="panel panel-default bootcards-media">
+        <div class="panel-body row">
+          <div class="row">
+            <div class="col-xs-2">
+              <img id="logo" src="https://github.com/taulantspahiu/Bootcards-Examples/blob/master/twitter_card/images/logo.jpg?raw=true">
+            </div>
+            <div class="col-xs-10">
+              <div class="row">
+                <p id="title">
+                  SitePoint <span class="inTitle">@sitepointdotcom &middot; 23h</span>
+                </p>
+              </div>
+              <div class="row heading">
+                <h5>Using Android Text-to-Speech to Create a Smart Assistant: </h5>
+              </div>
+            </div>
+          </div>
+          <div class="row body-card">
+            <div class="col-xs-4">
+              <img src="https://github.com/taulantspahiu/Bootcards-Examples/blob/master/twitter_card/images/sitepoint.png?raw=true">
+            </div>
+            <div class="col-xs-8">
+              <h4>Using Android Text-to-Speech to Create a...</h4>
+              <h5>Theodor Pandeli creates an Android app with text-to-speech that answers all of life's most important questions.</h5>
+              <h5>sitepoint.com</h5>
+            </div>
+          </div>
+        </div>
+        <div class="panel-footer">
+          <p><i class="fa fa-reply" aria-hidden="true"></i></p>
+          <p><i class="fa fa-retweet" aria-hidden="true"></i> 3</p>
+          <p><i class="fa fa-heart" aria-hidden="true"></i> 4</p>
+          <p><i class="fa fa-ellipsis-h" aria-hidden="true"></i></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  body {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+}
+.row {
+  background-color: #fff;
+}
+#logo {
+  width: 48px;
+  height: 48px;
+  margin: 3px -6px 0 6px;
+}
+
+.panel-body {
+  padding-top: 5px;
+  padding-bottom: 0px;
+}
+
+.body-card {
+  border: 1px solid #E1E8ED;
+  margin: 5px 20px 0 60px;
+  border-radius: 5px;
+}
+
+.body-card > div {
+  padding-left: 0px;
+}
+
+.body-card > div > h4,
+h5 {
+  margin: 6px 0;
+}
+
+.body-card div img {
+  height: 123px;
+  border-radius: 3px 0 0 3px;
+}
+
+.body-card .col-xs-8 h5:nth-child(3) {
+  color: #8899A6;
+}
+
+.panel-footer {
+  background-color: white;
+  border-color: white;
+  padding-bottom: 7px;
+}
+
+.panel-footer p {
+  display: inline-block;
+  margin: 0px 60px 5px 10px;
+  color: #aab8c2;
+}
+
+.heading h5 {
+  color: #292f33;
+  font-size: 16px;
+}
+
+#title {
+  margin-top: 1px;
+  margin-bottom: 0px;
+  font-weight: bold;
+  color: #292f33;
+}
+
+.inTitle {
+  color: #8899a6;
+  font-size: 13px;
+  font-weight: 200;
+}
+
+.panel-footer p:first-child:hover {
+  color: #2389AE;
+}
+
+.panel-footer p:nth-child(2):hover {
+  color: #19CF86;
+}
+
+.panel-footer p:nth-child(3):hover {
+  color: #E81C4F;
+}
+
+.panel-footer p:nth-child(4):hover {
+  color: #2389AE;
+}
+
+@media only screen and (max-width: 480px) {
+  .body-card {
+    margin: 0 10px 0 10px;
+    height: 123px;
+  }
+  .body-card > .col-xs-4 {
+    visibility: hidden;
+    display: none;
+  }
+  .body-card > .col-xs-8 {
+    width: 98%;
+    margin-left: 3px;
+  }
+  .panel-body > .row:first-child > div {
+    display: inline-block;
+  }
+  .panel-body > .row:first-child > .col-xs-10 {
+    width: 70%;
+  }
+  .panel-footer > p {
+    margin-right: 25px;
+  }
+  .fa-reply {
+    margin-left: 3px !important;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .panel-body > .row:first-child > .col-xs-2,
+  .panel-footer > p.first-child > i {
+    width: 75px;
+  }
+  .fa-reply {
+    margin-left: 35px;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1199px) {
+  .fa-reply {
+    margin-left: 38px;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  .panel-body > .row:first-child > .col-xs-2 {
+    width: 13%;
+  }
+  .fa-reply {
+    margin-left: 35px;
+  }
+}
+</style>
---
+++ components/data-table.vue
@@ -0,0 +1,159 @@
+<template>
+  <v-data-table :headers="headers" :items="desserts" class="elevation-1" item-key="name">
+    <template slot="items" slot-scope="props">
+      <tr @click="props.expanded = !props.expanded">
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-right">{{ props.item.calories }}</td>
+        <td class="text-xs-right">{{ props.item.fat }}</td>
+        <td class="text-xs-right">{{ props.item.carbs }}</td>
+        <td class="text-xs-right">{{ props.item.protein }}</td>
+        <td class="text-xs-right">{{ props.item.iron }}</td>
+      </tr>
+    </template>
+    <template slot="expand" slot-scope="props">
+      <div class="container-card">
+        <blockquote class="twitter-tweet" data-lang="es">
+          <p lang="en" dir="ltr">Working an application in Vue.js with TDD — An extensive guide for people who have time — part 2 <a href="https://t.co/LblVO1wKcg">https://t.co/LblVO1wKcg</a> <a href="https://twitter.com/hashtag/VueJS?src=hash&amp;ref_src=twsrc%5Etfw">#VueJS</a> <a href="https://twitter.com/hashtag/tdd?src=hash&amp;ref_src=twsrc%5Etfw">#tdd</a> <a href="https://twitter.com/hashtag/testing?src=hash&amp;ref_src=twsrc%5Etfw">#testing</a> <a href="https://twitter.com/hashtag/javascript?src=hash&amp;ref_src=twsrc%5Etfw">#javascript</a></p>&mdash; Vue.js News (@VueJsNews) <a href="https://twitter.com/VueJsNews/status/1053875276487245824?ref_src=twsrc%5Etfw">21 de octubre de 2018</a>
+        </blockquote>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+      </div class="container-card">
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import CardTwiiter from '@/components/card-twitter'
+export default {
+  components: {
+    CardTwiiter
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Nick Usuario',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' }
+      ],
+      desserts: [
+        {
+          value: false,
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        },
+        {
+          value: false,
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+          text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container-card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
